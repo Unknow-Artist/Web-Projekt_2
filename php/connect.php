0f3dae74e956f5aa5b9772d2a83e713320847df9
Lukas <62798936+3verywhere@users.noreply.github.com>
2022-05-20 09:58:03 +0200

    commit

--- php/connect.php
+++ php/connect.php
@@ -1,12 +1,12 @@
 <?php 
 $dsn = 'mysql:host=' . $host . ';dbname=' . $database;
 $options = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];
-try{
+try {
 	$db = new PDO($dsn, $user, $password, $options);
 	$db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
 	echo '<p>Verbindung fehlgeschlagen!</p>';
-	if(ini_get('display_errors')){
+	if (ini_get('display_errors')) {
 		echo '<p>' . $e -> getMessage();
 	}
 	exit;
