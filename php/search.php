f9d5590a7ae8d87642769f0a1d553517daee9f70
Lukas <62798936+3verywhere@users.noreply.github.com>
2022-06-03 08:17:39 +0200

    update

--- php/search.php
+++ php/search.php
@@ -4,15 +4,20 @@ require_once __DIR__ . '/functions.php';
 $db = getDb();
 $username = $_GET['username'];
 
-$statement = $db -> prepare("SELECT id, username FROM user");
-$statement -> execute();
-$userList = $statement -> fetch();
+if (empty($username)) {
+    $userList = $db -> prepare("SELECT id, username FROM user");
+} else {
+    $userList = $db -> prepare("SELECT id, username FROM user WHERE username LIKE :username");
+    $userList -> bindParam(':username', '%' . $username . '%');
+}
+
+$userList -> execute();
 
 foreach ($userList as $user) {
-    echo <<<EOT
-    <li>
+    echo <<<search
+    <li class="list-group-item d-flex justify-content-between align-items-center">
         $user[username]
     </li>
-    EOT;
+    search;
 }
 ?>
